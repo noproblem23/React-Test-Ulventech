d67189945ac9135b8987c473d8d46a7e3fe70071
icando1224 <48373101+icando1224@users.noreply.github.com>
2023-08-08 05:26:21 +0800

    f
    f

--- src/__test__/reducers.test.js
+++ src/__test__/reducers.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
-import { fetchUsers } from "../actions";
+import { fetchUsers, deleteUser } from "../actions";
 
 const mockStore = configureMockStore([thunk]);
 
@@ -8,9 +8,12 @@ describe("fetchUsers action", () => {
   it("should fetch users and dispatch actions", async () => {
     const store = mockStore();
 
-    await store.dispatch(fetchUsers());
+    await store.dispatch(fetchUsers()); //fetching test
+    await store.dispatch(deleteUser(1)); //deletion test
 
     const actions = store.getActions();
+
     expect(actions).toContainEqual({ type: "IS_LOADING", isLoading: true });
+    expect(actions).toContainEqual({ type: "DELETE_USER", userId: 1 });
   });
 });
